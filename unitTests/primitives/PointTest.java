92e4d25b310ef66d0f394ae09ec9ff885f1cdb55
dotz6000 <ohev.david@gmail.com>
2023-03-27 17:34:34 +0300

    adding Junit Tests

---
+++ unitTests/primitives/PointTest.java
@@ -0,0 +1,32 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void testSubtract() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testDistanceSquared() {
+    }
+
+    @Test
+    void testDistance() {
+    }
+
+    @Test
+    void testTestEquals() {
+    }
+
+    @Test
+    void testTestToString() {
+    }
+}
