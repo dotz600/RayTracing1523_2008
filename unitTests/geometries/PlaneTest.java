8719e627a8eaf2dcd6b0a480ab3eb6c492d20acd
Marcos <mordechay0@gmail.com>
2023-03-30 16:24:32 +0300

    All tests passed.

--- unitTests/geometries/PlaneTest.java
+++ unitTests/geometries/PlaneTest.java
@@ -21,7 +21,7 @@ class PlaneTest {
         //============ Equivalence Partitions Tests ==============
         Plane tst = new Plane(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0));
         assertEquals(1 , tst.getNormal().length(),0.00001, "ERROR: Normal Length wrong value");
-        double res = 1 + 1.0/3.0;
+        double res = 1 + 1.0/Math.sqrt(3);
         assertEquals(new Vector(res, res, res),
                 tst.getNormal(new Point(1, 1, 1)), "ERROR: Normal Length wrong value");
     }
