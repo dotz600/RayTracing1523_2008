7c11bb5f5d1014cdfebffbcb7c88ef62a2b214c5
dotz6000 <ohev.david@gmail.com>
2023-04-21 01:48:54 +0300

    Triangle findIntersections test

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -2,7 +2,9 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,17 +12,6 @@ import java.util.List;
  * by geometry with 3 points
  */
 public class Triangle extends Polygon {
-
-    /**
-     * Gets a ray and returns intersection points between the ray and the geometry.
-     *
-     * @param ray (not NULL)
-     * @return List of points if any. else NULL
-     */
-    @Override
-    public List<Point> findIntersections(Ray ray) {
-        return null;
-    }
 
     /**
      * constructor with 3 points of the triangle, using super constructor
@@ -31,4 +22,45 @@ public class Triangle extends Polygon {
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1, p2, p3);
     }
+
+    /**
+     * Gets a ray and returns intersection points between the ray and the geometry.
+     *
+     * @param ray (not NULL)
+     * @return List of points if any. else NULL
+     */
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+
+        // check if the ray intersect the plane of the triangle
+        List<Point> points = plane.findIntersections(ray);
+        if (points == null)
+            return null;
+
+        // check if the intersection point is inside the triangle
+        //create vectors from the intersection point to the vertices
+        //(pi - p(i+1) % vertices.size()) X (pi - p0)
+        ArrayList<Vector> vectors = new ArrayList<>();
+        for (int i = 0; i < vertices.size(); i++) {
+            try {
+                Vector v = vertices.get((i + 1) % vertices.size()).subtract(vertices.get(i));
+                Vector u = ray.getP0().subtract(vertices.get(i));
+                vectors.add(v.crossProduct(u));
+            } catch (IllegalArgumentException e) {
+                return null;
+            }
+        }
+
+        //check all the vector are in the same direction
+        //if not - the point is outside the triangle - return null
+        //TODO - make normal on time and dont trust getNormal() because the direction is important
+        for (int i = 0; i < vectors.size()-1;  i++)
+            if (vectors.get(i).dotProduct(this.plane.getNormal()) <= 0)
+                return null;
+
+        return points;
+    }
+
+
+
 }
--- unitTests/geometries/TriangleTest.java
+++ unitTests/geometries/TriangleTest.java
@@ -2,6 +2,7 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 
 import java.util.List;
@@ -43,10 +44,39 @@ class TriangleTest {
     @Test
     public void testFindIntersections() {
 
-        Point[] pts =
-                {new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0)};
-        Triangle triangle = new Triangle(pts[0], pts[1], pts[2]);
+        Triangle triangle = new Triangle(new Point(1, 1, 0),
+                new Point(-1, 1, 0), new Point(0, 3, 0));
+
+        //==============equivalence partition tests=================
+        //TC01: Ray's line is inside the triangle (1 point)
+        assertEquals(List.of(new Point(0, 2.5, 0)),
+                triangle.findIntersections(new Ray(new Point(0, 1.5, -1), new Vector(0, 1, 1))),
+                "Ray's line out of triangle");
+
+        assertEquals(List.of(new Point(0, 1.5, 0)),
+                triangle.findIntersections(new Ray(new Point(0, 1.5, -1), new Vector(0, 0, 1))),
+                "Ray's line out of triangle");
 
 
+        //TC02: Ray's line is outside against the edge (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(0, 0.5, 1), new Vector(0, -1, -1))),
+                "Ray's line should be out of triangle");
+
+        //TC03: Ray's line is outside against the vertex (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(1.5, 0.5, 1), new Vector(2, -1, -1))),
+                "Ray's line against vertex should be out of triangle");
+
+        //==============boundary values tests=================
+        //TC04: Ray's line intersection is in the vertex  (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(1, 1, -1), new Vector(0, 0, 1))),
+                "intersection on the edge should be out of triangle");
+
+        //TC05: Ray's line intersection is on the edge (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(0, 1, -1), new Vector(0, 0, 1))),
+                "intersection on the edge should be out of triangle");
+
+        //TC06: intersection is on the edge's continuation (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(2, 1, -1), new Vector(0, 0, 1))),
+                "intersection on the edge continuation should be out of triangle");
     }
 }
