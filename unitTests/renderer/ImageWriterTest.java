817b180fe93512cd6bcb91b4f166cf530a687404
dotz6000 <ohev.david@gmail.com>
2023-05-01 22:32:56 +0300

    creating our first image

--- unitTests/renderer/ImageWriterTest.java
+++ unitTests/renderer/ImageWriterTest.java
@@ -2,14 +2,32 @@ package renderer;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import primitives.Color;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageWriterTest {
     @Test
-    @Disabled
-    void klum(){
+    void writeToImageTests() {
 
+      int gridDensityW = 16, gridDensityH = 10;
+      ImageWriter image = new ImageWriter("myFirstImage", 800, 500);
+      int gridW = image.getNx()/gridDensityW ;
+      int gridH = image.getNy()/gridDensityH;
+
+      for (int i = 0; i < image.getNx(); i++)
+      {
+          for (int j = 0; j < image.getNy(); j++)
+          {
+              if(i % gridW == 0|| j % gridH == 0)//if its grid pixel color red
+                  image.writePixel(i,j, new Color(255, 0, 0));
+              else
+                image.writePixel(i,j,new Color(230, 255, 0));//yellow color
+          }
+      }
+      image.writeToImage();
     }
 
 }
